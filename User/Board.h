6d665738afd4a345b8999c6cbe7d91e178c78cdc
Tim Lamborn <tlamborn@gmail.com>
2021-05-09 00:47:41 +1200

    Initial board setup

--- User/Board.h
+++ User/Board.h
@@ -9,42 +9,45 @@
 //#define CORE_USE_TICK_IRQ
 
 // ADC config
-//#define ADC_VREF	        3300
+#define ADC_VREF	        3300
 
 // GPIO config
 //#define GPIO_USE_IRQS
 //#define GPIO_IRQ0_ENABLE
 
 // TIM config
-//#define TIM_USE_IRQS
-//#define TIM2_ENABLE
+#define TIM_USE_IRQS
+#define TIM22_ENABLE
 
 // UART config
-//#define UART1_GPIO		GPIOA
-//#define UART1_PINS		(GPIO_PIN_9 | GPIO_PIN_10)
-//#define UART1_AF		    GPIO_AF4_USART1
-//#define UART_BFR_SIZE     128
+#define UART1_GPIO		GPIOA
+#define UART1_PINS		(GPIO_PIN_9 | GPIO_PIN_10)
+#define UART1_AF		    GPIO_AF4_USART1
 
-// SPI config
-//#define SPI1_GPIO		    GPIOB
-//#define SPI1_PINS		    (GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5)
-//#define SPI1_AF			GPIO_AF0_SPI1
+// MP6532
+#define MP6532_GPIO			GPIOA
+#define MP6532_HA_PIN		GPIO_PIN_0
+#define MP6532_HB_PIN		GPIO_PIN_1
+#define MP6532_HC_PIN		GPIO_PIN_2
+#define MP6532_SLEEP_PIN	GPIO_PIN_3
+#define MP6532_DIR_PIN		GPIO_PIN_4
+#define MP6532_BRAKE_PIN	GPIO_PIN_5
+#define MP6532_PWM_PIN		GPIO_PIN_6
+#define MP6532_PWM_TIM		TIM_22
+#define MP6532_PWM_CH		0
+#define MP6532_PWM_AF		GPIO_AF5_TIM22
+#define MP6532_FAULT_PIN	GPIO_PIN_7
 
-// I2C config
-//#define I2C1_GPIO			GPIOB
-//#define I2C1_PINS			(GPIO_PIN_6 | GPIO_PIN_7)
-//#define I2C1_AF			GPIO_AF1_I2C1
-//#define I2C_USE_FASTMODEPLUS
+// Current sense
+#define ISNS_AIN			ADC_CHANNEL_8
+#define ISNS_GAIN			20
 
-// CAN config
-//#define CAN_GPIO			GPIOB
-//#define CAN_PINS			(GPIO_PIN_8 | GPIO_PIN_9)
-//#define CAN_AF			GPIO_AF4_CAN
-//#define CAN_DUAL_FIFO
+// COMP config
+#define COMP2_ENABLE
 
-// USB config
-//#define USB_ENABLE
-//#define USB_CLASS_CDC
-//#define USB_CDC_BFR_SIZE	512
+// LEDs
+#define LED_GPIO			GPIOC
+#define LED_GRN_PIN			GPIO_PIN_14
+#define LED_RED_PIN			GPIO_PIN_15
 
 #endif /* BOARD_H */
