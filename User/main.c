23521e43b2130df76bb7d09576a247d7df8d5a91
Lambosaurus <tlamborn@gmail.com>
2021-05-10 13:02:39 +1200

    Added debug settings, and setup up LEDs

--- User/main.c
+++ User/main.c
@@ -1,12 +1,14 @@
 
 #include "Core.h"
-
+#include "GPIO.h"
 
 
 
 int main(void)
 {
 	CORE_Init();
+	GPIO_EnableOutput(LED_GPIO, LED_GRN_PIN, GPIO_PIN_SET);
+	GPIO_EnableOutput(LED_GPIO, LED_RED_PIN, GPIO_PIN_RESET);
 
 	while(1)
 	{
